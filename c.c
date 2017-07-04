b7e1ad806b1c8cfc1e279fbc14833cd699b48374
Lursun <lursun914013@gmail.com>
2017-07-04 23:36:18 +0800

    decode block decode.py

--- c.c
+++ c.c
@@ -47,7 +47,7 @@ typedef struct BLOCKFILE{
   BYTE magic[4];
   BYTE blocksize[4];
   BLOCK *block;
-  BYTE space[8];
+  BYTE space[4];
 }BLOCKFILE;
 
 BYTE* varint(FILE *f);
@@ -60,12 +60,66 @@ TXINFO *getTxInfo(FILE *f);
 TX *getTx(FILE *f);
 void getTxInput(FILE *f,TXINPUT *input);
 void getTxOutput(FILE *f,TXOUTPUT *output);
+void showBlockFile(BLOCKFILE *blockfile);
+
 int main(int argc,char **args)
 { 
   FILE *f=fopen(*(args+1),"rb");
-  getBlockFile(f);
+  BLOCKFILE *blockfile;
+  long i=0;
+  while(feof(f)!=EOF){
+    i++;
+    blockfile=getBlockFile(f);
+    showBlockFile(blockfile);
+    printf("\nblock:%lld",i);
+    printf("\neof:%d\n",feof(f));
+  }
   
+
   return 0;
+}
+void showBlockFile(BLOCKFILE *blockfile){
+  printf("magic value:");
+  for (int i=0;i<4;i++) printf("%x",blockfile->magic[i]);
+  printf("\n");
+  printf("blocksize:%d bytes\n",*(int*)blockfile->blocksize);
+  printf("======block header======\n");
+  printf("version:%d\n",*(int*)blockfile->block->blockheader->version );
+  printf("previousHash:");
+  for(int i=31 ; i>=0 ; i--)printf("%02x",blockfile->block->blockheader->previousHash[i]);
+  printf("\nmerkleHash:");
+  for(int i=31 ; i>=0 ; i--)printf("%02x",blockfile->block->blockheader->merkleHash[i]);
+  printf("\ntimestamp:%d\n",(*(int*)blockfile->block->blockheader->timestamp) );
+  printf("bits:%x(%d)\n",*(int*)blockfile->block->blockheader->bits,*(int*)blockfile->block->blockheader->bits);
+  printf("nonce:%lld\n",*(int64_t*)blockfile->block->blockheader->nonce);
+  printf("========tx=========\n");
+  printf("tx count:%lld\n", decodeVarint(blockfile->block->blockbody->txcount) ) ;
+  for (int i=0;i<decodeVarint(blockfile->block->blockbody->txcount);i++){
+    printf("========tx%d=========\n",i);
+    printf("tx version:%d\n", *(int *)blockfile->block->blockbody->txs[i].version );
+    printf("tx input count:%lld\n",decodeVarint(blockfile->block->blockbody->txs[i].numinputs) );
+    for(int j=0;j<decodeVarint(blockfile->block->blockbody->txs[i].numinputs);j++){
+      printf("========tx %d input %d=========\n",i,j);
+      printf("previous_output_hash:");
+      for(int k=31 ; k >=0 ; k--)printf("%02x",blockfile->block->blockbody->txs[i].inputs[j].previous_output_hash[k]);
+      printf("\nprevious_output_index:%ld\n",*(int *)(blockfile->block->blockbody->txs[i].inputs[j].previous_output_index));
+      printf("script_lenght:%lld\n",decodeVarint(blockfile->block->blockbody->txs[i].inputs[j].script_length));
+      printf("script:");
+      for(int k=0 ; k <decodeVarint(blockfile->block->blockbody->txs[i].inputs[j].script_length) ; k++)printf("%02x",blockfile->block->blockbody->txs[i].inputs[j].signature_script[k]);
+      printf("\nsequence:%ld\n",*(int *)blockfile->block->blockbody->txs[i].inputs[j].sequence);
+    }
+    for(int j=0;j<decodeVarint(blockfile->block->blockbody->txs[i].numoutputs);j++){
+      printf("========tx %d output %d=========\n",i,j);
+      printf("value:%lld\n",*(int64_t *)blockfile->block->blockbody->txs[i].outputs[j].value);
+      printf("pk_script_length:%lld\n",decodeVarint(blockfile->block->blockbody->txs[i].outputs[j].pk_script_length) );
+      printf("script:");
+      for(int k=0;k<decodeVarint(blockfile->block->blockbody->txs[i].outputs[j].pk_script_length);k++)printf("%02x",blockfile->block->blockbody->txs[i].outputs[j].pk_script[k]);
+      printf("\n");
+    }
+    printf("space:%x\n",*(int *)blockfile->space);
+    
+  }
+  
 }
 
 
@@ -99,29 +153,27 @@ BLOCK *getBlock(FILE *f){
   BLOCK *block=malloc(sizeof(BLOCK));
   block->blockheader=getBlockHeader(f);
   block->blockbody=getTxInfo(f);
-  
-  
   return block;
 }
 BLOCKHEADER *getBlockHeader(FILE *f){
   BLOCKHEADER *blockheader;
   blockheader=malloc(80);
-  printf("======block header======\n");
+  //printf("======block header======\n");
   fread(blockheader,80,1,f);
-  printf("version:%d\n",*((int*)blockheader->version) );
+  //printf("version:%d\n",*((int*)blockheader->version) );
 
-  printf("previousHash:");
+  //printf("previousHash:");
   for(int i=31 ; i>=0 ; i--){
-    printf("%02x",blockheader->previousHash[i]);
+    //printf("%02x",blockheader->previousHash[i]);
   }
-  printf("\nmerkleHash:");
+  //printf("\nmerkleHash:");
   for(int i=31 ; i>=0 ; i--){
-    printf("%02x",blockheader->merkleHash[i]);
+    //printf("%02x",blockheader->merkleHash[i]);
   }
-  printf("\n");
-  printf("timestamp:%d\n",(*(int*)blockheader->timestamp) );
-  printf("bits:%x(%d)\n",(*(int*)blockheader->bits),(*(int*)blockheader->bits) );
-  printf("nonce:%lld\n",(*(int64_t*)blockheader->nonce) );
+  //printf("\n");
+  //printf("timestamp:%d\n",(*(int*)blockheader->timestamp) );
+  //printf("bits:%x(%d)\n",(*(int*)blockheader->bits),(*(int*)blockheader->bits) );
+  //printf("nonce:%lld\n",(*(int64_t*)blockheader->nonce) );
   return (blockheader);
 }
 
@@ -129,11 +181,13 @@ BLOCKFILE *getBlockFile(FILE *f){
   BLOCKFILE *blockfile=malloc(sizeof(BLOCKFILE));
   blockfile->block=malloc(sizeof(BLOCK));
   fread(blockfile,8,1,f);
-  printf("magic value:%x\n",*((int*)blockfile->magic));
-  printf("blocksize:%d bytes\n",*((int*)blockfile->blocksize));
+  //printf("magic value:%x\n",*((int*)blockfile->magic));
+  //printf("blocksize:%d bytes\n",*((int*)blockfile->blocksize));
   blockfile->block=getBlock(f);
-  fread(blockfile->space,8,1,f);
-  printf("%d\n",*(int *)blockfile->space);
+  
+  if(feof(f)!=EOF)
+  fread(blockfile->space,4,1,f);
+  //printf("%x\n",*(char *)blockfile->space);
   return blockfile;
 }
 
@@ -141,67 +195,67 @@ TXINFO *getTxInfo(FILE *f){
   TXINFO *blockbody=malloc(sizeof(TXINFO));
   blockbody=malloc(sizeof(TXINFO));
   blockbody->txcount=varint(f);
-  printf("========tx=========\n");
-  printf("tx count:%lld\n", decodeVarint(blockbody->txcount) ) ;
-  blockbody->txs=malloc(sizeof(TX)*(*(int *)blockbody->txcount) );
+  //printf("========tx=========\n");
+  //printf("tx count:%lld\n", decodeVarint(blockbody->txcount) ) ;
+  blockbody->txs=malloc(sizeof(TX)*decodeVarint(blockbody->txcount) );
 
   for (int i=0;i<*(int *)blockbody->txcount;i++){
     fread( &(blockbody->txs[i]),4,1,f);
-    printf("tx version:%d\n", blockbody->txs[i].version ) ;
+    //printf("tx version:%d\n", *(int *)blockbody->txs[i].version ) ;
 
     blockbody->txs[i].numinputs=varint(f);
-    printf("tx input count:%lld\n",decodeVarint(blockbody->txs[i].numinputs) );
-    printf("========tx input=========\n");
+    //printf("tx input count:%lld\n",decodeVarint(blockbody->txs[i].numinputs) );
     
-    blockbody->txs[i].inputs=malloc(sizeof(TXINPUT)*(*(int *)(blockbody->txs[i].numinputs)) );
-    getTxInput(f,&blockbody->txs[i].inputs);
+    blockbody->txs[i].inputs=malloc(sizeof(TXINPUT)*decodeVarint(blockbody->txs[i].numinputs) );
+    for(int j=0;j<decodeVarint(blockbody->txs[i].numinputs);j++){
+      getTxInput(f,&blockbody->txs[i].inputs[j]);
+    }
 
-    printf("========tx output========\n");
     blockbody->txs[i].numoutputs=varint(f);
-    printf("tx output count:%lld\n",decodeVarint(blockbody->txs[i].numoutputs) );
-    blockbody->txs[i].outputs=malloc(sizeof(TXOUTPUT)*(*(int *)(blockbody->txs[i].numoutputs)) );
-    getTxOutput(f,&blockbody->txs[i].outputs);
+    //printf("tx output count:%lld\n",decodeVarint(blockbody->txs[i].numoutputs) );
+    blockbody->txs[i].outputs=malloc(sizeof(TXOUTPUT)*decodeVarint(blockbody->txs[i].numoutputs) );
+    for(int j=0;j<decodeVarint(blockbody->txs[i].numoutputs);j++){
+      getTxOutput(f,&blockbody->txs[i].outputs[j]);
+    }
   }
   return blockbody;
 }
 
 
 void getTxInput(FILE *f,TXINPUT *input){
-  printf("========tx input=========\n");
-  printf("previous_output_hash:");
+  //printf("========tx input=========\n");
+  //printf("previous_output_hash:");
   fread(input->previous_output_hash,32,1,f);
   for(int i=31 ; i>=0 ; i--){
-    printf("%02x",input->previous_output_hash[i]);
+    //printf("%02x",input->previous_output_hash[i]);
   }
-  printf("\n");
+  //printf("\n");
   fread(input->previous_output_index,4,1,f);
-  printf("previous_output_index:%ld\n",*(int *)(input->previous_output_index));
+  //printf("previous_output_index:%ld\n",*(int *)(input->previous_output_index));
   input->script_length=varint(f);
-  printf("script_lenght:%lld\n",decodeVarint(input->script_length));
+  //printf("script_lenght:%lld\n",decodeVarint(input->script_length));
   input->signature_script=readLength(decodeVarint(input->script_length),f);
-  printf("script:");
+  //printf("script:");
   for(int i=0;i<*(int *)(input->script_length);i++)
   {
-    printf("%02x",input->signature_script[i]);
+    //printf("%02x",input->signature_script[i]);
   }
-  printf("\n");
+  //printf("\n");
   fread( input->sequence,4,1,f);
-  printf("sequence:%ld\n",*(int *)input->sequence);
-  
+  //printf("sequence:%ld\n",*(int *)input->sequence);
 }
 
 void getTxOutput(FILE *f,TXOUTPUT *output){
-  printf("========tx output========\n");
-  /////////////////
+  //printf("========tx output========\n");
   fread(output->value,8,1,f);
-  printf("value:%lld\n",*(long long int *)output->value);
+  //printf("value:%lld\n",*(long long int *)output->value);
   output->pk_script_length=varint(f);
-  printf("pk_script_length:%lld\n",decodeVarint(output->pk_script_length) );
+  //printf("pk_script_length:%lld\n",decodeVarint(output->pk_script_length) );
   output->pk_script=readLength(decodeVarint(output->pk_script_length),f);
-  printf("script");
+  //printf("script");
   for(int i=0;i<*(int *)output->pk_script_length;i++)
   {
-    printf("%02x",output->pk_script[i]);
+    //printf("%02x",output->pk_script[i]);
   }
-  printf("\n");
+  //printf("\n");
 }
---
+++ decode.py
@@ -0,0 +1,156 @@
+import sys
+# BLOCKFILE *getBlockFile(FILE *f){
+#   BLOCKFILE *blockfile=malloc(sizeof(BLOCKFILE));
+#   blockfile->block=malloc(sizeof(BLOCK));
+#   fread(blockfile,8,1,f);
+#   //printf("magic value:%x\n",*((int*)blockfile->magic));
+#   //printf("blocksize:%d bytes\n",*((int*)blockfile->blocksize));
+#   blockfile->block=getBlock(f);
+  
+#   if(feof(f)!=EOF)
+#   fread(blockfile->space,4,1,f);
+#   //printf("%x\n",*(char *)blockfile->space);
+#   return blockfile;
+# }
+
+class varint:
+        def __init__(self,handle):
+            s=handle.read(1)
+            if ord( s )>=253:
+                s.join( handle.read(2<<(s&int('0x03',16)-1)) )
+            self.s=s
+        def __int__(self):
+            if ord(self.s[0]) >=253:
+                return int( ''.join('%02x'%ord( ch )for ch in self.s[1:] ) ,16)
+            return ord(self.s[0])
+        def __str__(self):
+            return self.s
+        def __repr__(self):
+            return ''.join('%02x'%ord( ch )for ch in self.s)
+            
+
+
+
+
+class BlockFile:
+    def __init__(self,filename):
+        self.file=file(filename,'rb')
+        self.txs=list()
+
+    def getBlockFile(self):
+        handle=self.file
+        self.magic=''.join('%02x'%ord( ch )for ch in handle.read(4))
+        self.blocksize=''.join('%02x'%ord( ch )for ch in handle.read(4))
+        self.getBlock()
+        #self.space=''.join('%02x'%ord( ch )for ch in handle.read(4))
+        
+    def getBlock(self):
+        self.getBlockHeader()
+        self.getBlockBody()
+
+    def getBlockHeader(self):
+        handle=self.file
+        self.version=''.join('%02x'%ord( ch )for ch in handle.read(4))
+        self.previousHash=''.join('%02x'%ord( ch )for ch in handle.read(32))
+        self.merkleHash=''.join('%02x'%ord( ch )for ch in handle.read(32))
+        self.timestamp=''.join('%02x'%ord( ch )for ch in handle.read(4))
+        self.bits=''.join('%02x'%ord( ch )for ch in handle.read(4))
+        self.nonce=''.join('%02x'%ord( ch )for ch in handle.read(4))
+
+    def getBlockBody(self):
+        self.getTxInfo()
+
+    def getTxInfo(self):
+        handle=self.file
+        self.txcount=varint(handle)
+        for i in range(int(self.txcount)):
+            self.txs.append(dict())
+            self.txs[i]['version']=''.join('%02x'%ord( ch )for ch in handle.read(4))
+            self.txs[i]['numinput']=varint(handle)
+            self.txs[i]['inputs']=list()
+            for j in range(int(self.txs[i]['numinput'])):
+                self.txs[i]['inputs'].append(self.getTxInput())
+            self.txs[i]['numoutput']=varint(handle)
+            self.txs[i]['outputs']=list()
+            for j in range(int(self.txs[i]['numoutput'])):
+                self.txs[i]['outputs'].append(self.getTxOutput())
+            self.txs[i]['space']=''.join('%02x'%ord( ch )for ch in handle.read(4))
+
+    def getTxInput(self):
+        handle=self.file
+        class TxInput:
+            def __init__(self):
+                self.previousOutputHash=''.join('%02x'%ord( ch )for ch in handle.read(32))
+                self.previousOutputIndex=''.join('%02x'%ord( ch )for ch in handle.read(4))
+                self.scriptLength=varint(handle)
+                self.signatureScript=''.join('%02x'%ord( ch )for ch in handle.read(int(self.scriptLength)))
+                self.sequence=''.join('%02x'%ord( ch )for ch in handle.read(4))
+            def __str__(self):
+                s=""
+                s+=(self.previousOutputHash)
+                s+=(self.previousOutputIndex)
+                s+=( str(self.scriptLength ) )
+                s+=(self.signatureScript)
+                s+=(self.sequence)
+                return s
+            def __repr__(self):
+                s=""
+                s+="   tx input previousOutputHash:%s\n" % self.previousOutputHash
+                s+="   tx input previousOutputIndex:%s\n" % self.previousOutputIndex
+                s+="   tx input scriptLength:%s\n" % repr(self.scriptLength)
+                s+="   tx input signatureScript:%s\n" % self.signatureScript
+                s+="   tx input sequence:%s\n" % self.sequence
+                return s
+        return TxInput()
+
+    def getTxOutput(self):
+        handle=self.file
+        class TxOutput:
+            def __init__(self):
+                self.value=''.join('%02x'%ord( ch )for ch in handle.read(8))
+                self.pkScriptLength=varint(handle)
+                self.pkScript=''.join('%02x'%ord( ch )for ch in handle.read(int(self.pkScriptLength)))
+            def __str__(self):
+                s=""
+                s+=(self.value)
+                s+=(str(self.pkScriptLength))
+                s+=(self.pkScript)
+                return s
+            def __repr__(self):
+                s=""
+                s+="   tx output value:%s\n" % self.value
+                s+="   tx output pkScriptLength:%s\n" % repr(self.pkScriptLength)
+                s+="   tx output pkScript:%s\n" % self.pkScript
+                return s
+        return TxOutput()
+    def __repr__(self):
+        s=""
+        s+="magic value:%s\n" % self.magic
+        s+="blocksize:%s\n" % self.blocksize
+        s+=" block version:%s\n" % self.version
+        s+=" block previousHash:%s\n" %self.previousHash
+        s+=" block merkleHash:%s\n" %self.merkleHash
+        s+=" block timestamp:%s\n" %self.timestamp
+        s+=" block bits:%s\n" %self.bits
+        s+=" block nonce:%s\n" %self.nonce
+        s+="  tx count:%s\n" %repr(self.txcount)
+        for i in range(int(self.txcount)):
+            s+="  tx version:%s\n" % self.txs[i]['version']
+            s+="  tx numinput:%s\n" % repr(self.txs[i]['numinput'])
+            for j in range(self.txs[i]['numinput']):
+                s+=repr(self.txs[i]['inputs'][j])
+            s+="  tx numoutput:%s\n" % repr(self.txs[i]['numoutput'])
+            for j in range(self.txs[i]['numoutput']):
+                s+=repr(self.txs[i]['outputs'][j])
+            s+="  tx space:%s\n" % self.txs[i]['space']
+        return s
+
+
+if __name__=="__main__":
+    
+    block=BlockFile(sys.argv[1])
+    for i in range(182):
+        block.getBlockFile()
+    print repr(block)
+    
+    
